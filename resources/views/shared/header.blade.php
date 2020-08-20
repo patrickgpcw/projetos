9aa509beaa350ef738f497162dbbc6159ef8cba0
Patrick C Pessanha <patrick.surfista@hotmail.com>
2020-08-20 14:16:29 -0300

    crianção das migrations e algumas modificações do html

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,29 @@
+<header class="flex items-center justfify-around bg-gray-900">
+    <div>
+        <h1>
+            Jogos
+        </h1>
+    </div>
+    <div>
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                Sobre o Projeto
+            </li>
+        </ul>
+    </div>
+    <div>
+        <div class="w-full min-w-0 lg:px-6 xl:px-12">
+            <div class="relative">
+                <input type="search" place-holder="Digite para pesquisar" name="" id="">
+                <div>
+                    <svg class="fill-current pointer-events-none text-gray-600 w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
